3eba2c05444f177b7bbdaa29736a91c651e97a22
Johnathan Hicks <fiesh27@users.noreply.github.com>
2020-10-30 15:55:56 -0600

    First Commit

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import app,db
+from app.models import User, Post
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.shell_context_processor
+def maske_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
