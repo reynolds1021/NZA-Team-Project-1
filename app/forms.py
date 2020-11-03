9dc3b5990f8b542e19985b90142458c5f778101b
Johnathan Hicks <fiesh27@users.noreply.github.com>
2020-11-02 19:02:14 -0700

    New Changes

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,11 @@ class LoginForm(FlaskForm):
     email = StringField ('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField()
+
+
+class UserInfoForm(FlaskForm):	
+    username = StringField('Username', validators=[DataRequired()])	   
+    email = StringField('Email', validators=[DataRequired(), Email()])	   
+    password = PasswordField('Password', validators=[DataRequired()])	    
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])	
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from werkzeug.security import check_password_hash
 from app.models import User
-
-from app.forms import UserInfoForm
+from flask_login import login_user
+from app.forms import UserInfoForm, LoginForm
 
 
 @app.route('/')
@@ -40,7 +40,26 @@ def register():
         return redirect(url_for('index'))
     return render_template('register.html', **context)
 
-@app.route('/attorneys')
-def attorneys():
+@app.route('/login')
+def login():
+    form = LoginForm()
 
-    return render_template ('attorneys.html')
+    context = {
+        'form': form
+    }
+    if request.method == 'POST' and form.validate():
+
+        username = form.username.data
+        # email = form.email.data
+        password = form.password.data
+        
+        logged_user = User.query.filter(username = username ).first()
+        if  logged_user and check_password_hash(logged_user.password, password):
+            login_user(logged_user)
+
+
+
+
+            flash("You have successfully logged in!", 'success')
+            return redirect(url_for('index'))
+    return render_template ('login.html', **context)
